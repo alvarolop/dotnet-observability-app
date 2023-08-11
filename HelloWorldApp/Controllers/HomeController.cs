ddeb1528320fcc79d98f0527bdbe49d88657fa84
Alvaro López Medina <alopezme@redhat.com>
2023-08-11 10:47:30 +0200

    :seedling: Add initial application with Prometheus metrics and Health checks

---
+++ HelloWorldApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+using System;
+using System.Diagnostics;
+
+namespace HelloWorldApp.Controllers
+{
+    [Controller]
+    public class HomeController : ControllerBase
+    {
+        // Create Prometheus metrics
+        private static readonly Counter HelloCounter = Metrics.CreateCounter("app_hello_counter_total", "Total requests to /hello endpoint");
+        private static readonly Histogram HelloHistogram = Metrics.CreateHistogram("app_hello_request_duration_seconds", "Request duration for /hello endpoint");
+
+        [HttpGet("/hello")]
+        public IActionResult GetHello()
+        {
+            // Increment the counter metric
+            HelloCounter.Inc();
+
+            // Measure the request duration
+            using (HelloHistogram.NewTimer())
+            {
+                string helloMessage = Environment.GetEnvironmentVariable("HELLO_MESSAGE") ?? "Alvaro";
+                return Ok($"Welcome to our .NET application, {helloMessage}!");
+            }
+        }
+
+        [HttpGet("/error")]
+        public IActionResult GetError()
+        {
+            try
+            {
+                throw new Exception("An error occurred!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,35 @@
+using Prometheus;
+using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using HelloWorldApp.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add controller services to the application's service collection.
+builder.Services.AddControllers();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Enable routing for the application.
+app.UseRouting();
+
+// Maps the controllers to endpoints based on their routes.
+app.MapControllers();
+
+app.UseHttpMetrics();
+
+app.MapHealthChecks("/healthz/live");
+
+app.MapHealthChecks("/healthz/ready");
+
+app.MapMetrics();
+
+app.Run("http://localhost:8080");
